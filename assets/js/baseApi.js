cfa12649110fd8703895a64a1c85efa56ebcdd99
lxh15 <1392649188@qq.com>
2022-06-11 09:59:12 +0800

    bug完善

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -9,7 +9,7 @@ $.ajaxPrefilter((options) => {
     if (options.url.includes('/my/')) {
         options.headers = { Authorization: localStorage.getItem('token') }
     };
-    option.complete = (res) => {
+    options.complete = (res) => {
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
             //  强制清空 token
